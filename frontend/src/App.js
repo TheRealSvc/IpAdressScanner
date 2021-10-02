968ff49c4725413fc9e6ff3513d0a13972e8fbd9
SVC <svcoelln@yahoo.de>
2021-10-02 21:36:44 +0200

    temporary bugs - go to dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,54 @@ import './css/index.css';
 import IpScanner from './components/IpScanner';
 import TestTable from './components/TestTable';
 import React, { Component } from 'react';
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 import {
     BrowserRouter,
     Route,
-    Switch,
+    Switch
   } from 'react-router-dom';
 
 /**
  * Main component for routing 
  */
 class App extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {computers: '', loading: 'true'} 
+  };
 
+  updateIpScan = () =>  {
+    console.log(this.state.loading) ;
+    this.setState({loading: 'true'}, () => {
+    fetch('http://localhost:5000/api/getcomputer')
+    .then ( x => x.json())
+    .then( x => { this.setState({computers: x, loading: 'false'})})
+    .catch( 'error in fetching computers') }
+    )
+  };
+
+  componentDidMount() {
+      this.updateIpScan() ;
+    } 
+   
     render() {
       return ( 
+        (this.state.loading==='true')
+       ?  <p> please wait while computers in local network are retrieved
+       <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={300}
+        width={300}
+        timeout={60000} // ms
+      /> </p>
+       :
         <BrowserRouter>    
         <div className="container"> 
             <Switch> 
-              <Route exact path= "/scanner" component={IpScanner} /> 
+              <Route exact path= "/" render={ () => <IpScanner computers={this.state.computers} rescanfun={this.updateIpScan } /> } /> 
               <Route exact path= "/test" component={TestTable} /> 
             </Switch> 
         </div>
--- frontend/src/components/IpScanner.jsx
+++ frontend/src/components/IpScanner.jsx
@@ -3,31 +3,25 @@ import { JsonToTable } from "react-json-to-table";
 //import { Button } from 'react-native-elements';
 
 class IpScanner extends Component { 
-    constructor(props) {
+    
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
         super(props);
-        this.state = {computers: ''} // [{Computername: '' , IPv4: '', MAC: '' }] }
     };
-
-    updateIpScan = () =>  {
-        fetch('http://localhost:5000/api/getcomputer')
-        .then ( x => x.json())
-        .then( x => { this.setState({computers: x})})
-        .catch( 'error in fetching computers') 
-      };
     
       componentDidMount() {
-      }   
+      }     
 
   render()  {
   return (
     <div className="ScanPage">
       <header className="MyHeader">
-        <button onClick={this.updateIpScan} >
-        Start Scan
+        <button onClick={this.props.rescanfun() } >
+        Update Scanning Table
         </button>
       </header>
      <p> </p>
-      <JsonToTable json={this.state.computers} />
+      <JsonToTable json={this.props.computers} />
     </div>   
   );
 }
